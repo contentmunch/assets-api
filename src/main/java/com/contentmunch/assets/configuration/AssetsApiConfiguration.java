6d423d90f079b098c81f757e0ac5384646180f73
asikpradhan@gmail.com <tsb-3DS-ngG-J9K>
2021-08-25 20:45:27 +0545

    feat: hateoas implementation

--- src/main/java/com/contentmunch/assets/configuration/AssetsApiConfiguration.java
+++ src/main/java/com/contentmunch/assets/configuration/AssetsApiConfiguration.java
@@ -1,13 +1,15 @@
 package com.contentmunch.assets.configuration;
 
 import com.contentmunch.assets.controller.AssetsController;
+import com.contentmunch.assets.data.AssetAssembler;
 import com.contentmunch.assets.exception.AssetsApiExceptionAdvice;
+import com.contentmunch.assets.service.AssetService;
 import com.contentmunch.assets.service.GoogleDriveService;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
-@ComponentScan(basePackageClasses = {AssetsController.class, AssetsApiExceptionAdvice.class, GoogleDriveService.class, AssetDriveConfig.class})
+@ComponentScan(basePackageClasses = {AssetsController.class, AssetsApiExceptionAdvice.class, AssetAssembler.class, AssetService.class, GoogleDriveService.class, AssetDriveConfig.class})
 @Configuration
 @PropertySource(value = "classpath:assets-api-application.yml", factory = YamlPropertySourceFactory.class)
 public class AssetsApiConfiguration {
