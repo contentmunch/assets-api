d3b088e22d0efd0b565747f258a5e1ceecd0eccb
contentmuncher <contentmunch@gmail.com>
2025-03-22 21:09:35 -0400

    feat: put or post, with file id

--- src/main/java/com/contentmunch/assets/external/GoogleDriveVideoService.java
+++ src/main/java/com/contentmunch/assets/external/GoogleDriveVideoService.java
@@ -172,7 +172,7 @@ public class GoogleDriveVideoService implements VideoService {
             var video = findVideoBy(folderId, name);
 
             HttpRequest request = video.isPresent() ?
-                    requestFactory.buildPatchRequest(new GenericUrl(String.format("https://www.googleapis.com/upload/drive/v3/files/{%s}?uploadType=resumable", video.get().id())),
+                    requestFactory.buildPutRequest(new GenericUrl(String.format("https://www.googleapis.com/upload/drive/v3/files/{%s}?uploadType=resumable", video.get().id())),
                             new JsonHttpContent(getDefaultInstance(), fileMetadata))
                     :
                     requestFactory.buildPostRequest(new GenericUrl(String.format(UPLOAD_URL_FORMAT, "?uploadType=resumable")),
