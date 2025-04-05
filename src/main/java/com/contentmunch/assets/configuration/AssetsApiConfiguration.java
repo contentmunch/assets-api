cc8e51dd5fb9fbb4fbd81dbd3ed3d9630a238db1
contentmuncher <contentmunch@gmail.com>
2025-04-04 20:49:54 -0400

    feat: no cache for html

--- src/main/java/com/contentmunch/assets/configuration/AssetsApiConfiguration.java
+++ src/main/java/com/contentmunch/assets/configuration/AssetsApiConfiguration.java
@@ -5,6 +5,7 @@ import com.contentmunch.assets.controller.AssetsController;
 import com.contentmunch.assets.data.assembler.AssetAssembler;
 import com.contentmunch.assets.data.assembler.AssetFolderAssembler;
 import com.contentmunch.assets.exception.AssetsApiExceptionAdvice;
+import com.contentmunch.assets.filter.HtmlNoCacheFilter;
 import com.contentmunch.assets.service.AssetService;
 import com.contentmunch.assets.external.GoogleDriveService;
 import org.springframework.cache.annotation.EnableCaching;
@@ -15,7 +16,7 @@ import org.springframework.retry.annotation.EnableRetry;
 
 @ComponentScan(basePackageClasses = {AssetController.class, AssetsController.class,
         AssetsApiExceptionAdvice.class, AssetFolderAssembler.class, AssetAssembler.class,
-        AssetService.class, GoogleDriveService.class, AssetDriveConfig.class})
+        AssetService.class, GoogleDriveService.class, AssetDriveConfig.class, HtmlNoCacheFilter.class})
 @Configuration
 @PropertySource(value = "classpath:assets-api-application.yml", factory = YamlPropertySourceFactory.class)
 @EnableCaching
