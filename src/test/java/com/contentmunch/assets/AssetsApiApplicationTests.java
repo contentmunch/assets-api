c21f1a0305a5b3cc30b387ff6bd3c4671f249e68
contentmuncher <contentmunch@gmail.com>
2025-04-04 21:13:03 -0400

    feat: no cache for html

--- src/test/java/com/contentmunch/assets/AssetsApiApplicationTests.java
+++ src/test/java/com/contentmunch/assets/AssetsApiApplicationTests.java
@@ -1,18 +1,14 @@
 package com.contentmunch.assets;
 
-import com.contentmunch.assets.service.VideoService;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AssetsApiApplicationTests {
-    @Autowired
-    private VideoService videoService;
 
     @Test
     void contextLoads() {
-        System.out.println(videoService.findVideoBy("18kPGSakofB6DiKlzqI1SBSWXlg4iYtKQ", "cover-video"));
+
     }
 
 }
